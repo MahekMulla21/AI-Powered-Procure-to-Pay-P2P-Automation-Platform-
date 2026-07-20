7c8a67ead62251e3cb57d172b61ec81dfd00ec01
Mahek Mulla <Mahekmulla491@gmail.com>
2026-07-21 02:36:42 +0530

    Updated database connection for Render PostgreSQL

--- frontend/db.py
+++ frontend/db.py
@@ -1,12 +1,15 @@
 import psycopg2
 
+import os
+import psycopg2
+
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="clrvw_db",
-        user="postgres",
-        password="Aleeza@123"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "5432")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
  
 # def save_file_record(file_name, file_path, file_type, file_size, status, reason):
